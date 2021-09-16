ec2f7f91ff238cc40550aaebf3d29464ce98f5fb
Terry Castonguay <terrycastonguay911@gmail.com>
2021-09-16 09:07:13 -0400

    Pushing Character controller
    movement script
    animation
    Input System

--- Project knight/Assets/Scripts/InputManager.cs
+++ Project knight/Assets/Scripts/InputManager.cs
@@ -1,28 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine;
+//using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public float speed;
-
-    [HideInInspector] public Vector3 rawInputMovement;   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(rawInputMovement * Time.deltaTime * speed);
-    }
+    [HideInInspector] public Vector3 rawInputMovement;
 
     public void OnMovement(InputAction.CallbackContext value)
     {
-        Vector2 inputMovement = value.ReadValue<Vector2>();
-        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
-    }
+       Vector2 inputMovement = value.ReadValue<Vector2>();
+       rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
+    } 
 }
--- Project knight/Assets/Scripts/PlayerController.cs
+++ Project knight/Assets/Scripts/PlayerController.cs
@@ -1,40 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] InputManager input;
+    [SerializeField] Rigidbody rigid;
+    Vector2 inputMovement = Vector2.zero;
+    [SerializeField] Vector3 rawInputMovement;
+    [SerializeField] Animator anim;
+    [SerializeField] SpriteRenderer sprite;
+    [SerializeField] bool isFacingRight;
 
-    public float speed;
-    public Rigidbody rigid;
+    [SerializeField] float speed;
 
-    // Start is called before the first frame update
-    void Start()
+    public void OnMovement(InputAction.CallbackContext context)
     {
-        Initiate();
+        //Read the input value from a controller.
+        inputMovement = context.ReadValue<Vector2>();
+        //Re-direct the Vector2 input to a Vector3 for 3D movement.
+        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y).normalized * speed;
+
+        Flip(inputMovement.x);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        OnMovement();
+        isFacingRight = true;
     }
 
-    private void OnMovement()
+    private void FixedUpdate()
     {
-        transform.Translate(input.rawInputMovement * Time.deltaTime * speed);
+        //Move Character base on input.
+        rigid.MovePosition(rigid.position + rawInputMovement * Time.deltaTime);
     }
 
-    private void Initiate()
+    private void Update()
     {
-        //Check to see if player has it's components.
-        if (input == null)
+        //check to see if player is moving.
+        float movement = rawInputMovement.sqrMagnitude;
+        //set the float (movement) animation to the speed of the character.
+        anim.SetFloat("movement", movement);
+    }
+
+    private void Flip(float direction)
+    {
+        if (direction > 0 && !isFacingRight || direction < 0 && isFacingRight)
         {
-            input = FindObjectOfType<InputManager>();
-            Debug.Log("Found inputManager");
+            isFacingRight = !isFacingRight;
+
+            Vector3 scaling = transform.localScale;
+
+            scaling.x *= -1;
+
+            transform.localScale = scaling;
         }
-        else
-            Debug.Log("Could not find InputManager script, are you missing a reference?");
+
     }
 }
